817252f822392595726c1cac6ba4b1e0fa1b2377
asanga <asanga.manage@auspost.com.au>
2020-06-03 14:30:35 +1000

    Added more steps to readme

--- init_config_command.sh
+++ init_config_command.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-npm run config -- --account-id=789775884885 --bucket-name="asangam" --region="ap-southeast-2" --function-name="asangamServerless"
+npm run config -- --account-id=789775884885 --bucket-name="YOUR_BUCKET_NAME" --region="YOUR_AWS_REGION" --function-name="YOUR_FUNCTION_NAME"
